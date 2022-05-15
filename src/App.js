08db27b16044f72aaf4a3aa39e32a6b76549b62b
arongalvan <arongalvan03.hotmail.com>
2022-05-15 14:10:05 -0700

    Add default Boeing

--- src/App.js
+++ src/App.js
@@ -14,28 +14,18 @@ import alaskaTailLogo from './images/alaska-airlines-logo-tail.png';
 import alaskaLogo from './images/alaska-airlines-logo-alt.png';
 import qatarTailLogo from './images/qatar-airways-logo-tail.png';
 import qatarLogo from './images/qatar-airways-logo-alt.png';
+import boeingTailLogo from './images/boeing-logo-tail.png';
+import boeingLogo from './images/boeing-logo.png';
 
 function App() {
 
   const [toggled, setToggled] = useState (false);
-  const [airlineTailLogo, setAirlineTailLogo] = useState ();
-  const [airlineLogo, setAirlineLogo] = useState ();
+  const [airlineTailLogo, setAirlineTailLogo] = useState (boeingTailLogo);
+  const [airlineLogo, setAirlineLogo] = useState (boeingLogo);
 
   const handleClick = () => {
       setToggled((s) => !s);
   };
-
-  useEffect(() => {
-    LoadAirlineColors();
-  }, []);
-
-  const LoadAirlineColors = () => {
-    document.documentElement.style.setProperty('--airline-primary-color', `${airlineColors['airline-primary-color']}`);
-    document.documentElement.style.setProperty('--airline-accent-color-1', `${airlineColors['airline-accent-color-1']}`);
-    document.documentElement.style.setProperty('--airline-accent-color-2', `${airlineColors['airline-accent-color-2']}`);
-    document.documentElement.style.setProperty('--airline-accent-color-3', `${airlineColors['airline-accent-color-3']}`);
-    document.documentElement.style.setProperty('--airline-accent-color-4', `${airlineColors['airline-accent-color-4']}`);
-  }
 
   const handleAirlineClick = (e) => {
     if(e.target.value == 1)
@@ -71,21 +61,33 @@ function App() {
       setAirlineTailLogo(virginTailLogo);
       return;
     }
+    if(e.target.value == 4)
+    {
+      document.documentElement.style.setProperty('--airline-primary-color', `${airlineColors.Boeing['airline-primary-color']}`);
+      document.documentElement.style.setProperty('--airline-accent-color-1', `${airlineColors.Boeing['airline-accent-color-1']}`);
+      document.documentElement.style.setProperty('--airline-accent-color-2', `${airlineColors.Boeing['airline-accent-color-2']}`);
+      document.documentElement.style.setProperty('--airline-accent-color-3', `${airlineColors.Boeing['airline-accent-color-3']}`);
+      document.documentElement.style.setProperty('--airline-accent-color-4', `${airlineColors.Boeing['airline-accent-color-4']}`);
+      setAirlineLogo(boeingLogo);
+      setAirlineTailLogo(boeingTailLogo);
+      return;
+    }
   };
 
   return (
     <>
     <Container id='header-wrapper'>
       <Row id='header'>
-        <SearchBar airlineLogo={airlineLogo}></SearchBar>
-        <Form.Group id="filters-form-group">
+      <Form.Group id="filters-form-group">
             <Form.Label className="filters-form-label" style={{color:"var(--airline-primary-color)"}}>Airline</Form.Label>
             <Form.Select className="filters-form-select">
                 <option className="filters-form-option" value="1" onClick={handleAirlineClick}>Qatar</option>
                 <option className="filters-form-option" value="2" onClick={handleAirlineClick}>Alaska</option>
                 <option className="filters-form-option" value="3" onClick={handleAirlineClick}>Virgin</option>
+                <option className="filters-form-option" value="4" onClick={handleAirlineClick}>Boeing</option>
             </Form.Select>
         </Form.Group>
+        <SearchBar airlineLogo={airlineLogo}></SearchBar>
       </Row>
     </Container>
     <Container id="content-wrapper">
